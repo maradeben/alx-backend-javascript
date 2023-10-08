f11986a72e6ed8332552bf23eb9499d3315fa607
Eben <maradebenezer@gmail.com>
2023-10-08 04:56:54 +0100

    fixing 7 and 9

--- 0x01-ES6_promise/9-try.js
+++ 0x01-ES6_promise/9-try.js
@@ -1,13 +1,13 @@
 export default function guardrail(mathFunction) {
   const queue = [];
-  let mathFunc;
 
   try {
-    mathFunc = mathFunction();
-  } catch (error) {
-    mathFunc = error.toString();
+    queue.push(mathFunction());
+  } catch (err) {
+    queue.push(String(err));
+  } finally {
+    queue.push('Guardrail was processed');
   }
-  queue.push(mathFunc);
-  queue.push('Guardrail was processed');
+
   return queue;
 }
